43b7288ee12b46056b5dadc76a0d3012d9656911
surya-1310 <ssur.it2024@rmd.ac.in>
2026-06-07 20:30:26 +0530

    Add files via upload

---
+++ StringCombinations.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StringCombinations {
+
+    static void generate(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String remaining = str.substring(0, i) + str.substring(i + 1);
+            generate(remaining, ans + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Possible combinations:");
+        generate(str, "");
+
+        sc.close();
+    }
+}
